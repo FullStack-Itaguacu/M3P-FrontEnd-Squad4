7734271139622f6b948250dd2934daa3b5b7dac3
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2023-10-18 09:40:39 -0300

    feat: base campo de busca

--- src/pages/MarketplacePage.jsx
+++ src/pages/MarketplacePage.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 
 const MarketplacePage = () => {
+  const [filterSearch, setFilterSearch] = useState('');
+
+  const updateFilter = (nameFilter, valueFilter) => {
+    if (nameFilter === 'newValueSearch') {
+      setFilterSearch(valueFilter);
+    }
+  };
+
   return (
     <div>
-    <Navbar/>
+      <Navbar changeSearch={updateFilter} />
     </div>
-  )
+    // <exemplo meio changeSearch={updateFilter} /> rever isso quando tiver pronto os cards e apagar comentario
+  );
 }
 
-export default MarketplacePage
+export default MarketplacePage;
