5705b106c7ade7b89dd36ca1c53885dc8f0cfd5b
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2023-10-17 22:43:47 -0300

    style: add slider no layout marketpage

---
+++ src/components/Slider.jsx
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import { sliderItems } from "../data";
+import { useState } from "react";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 110px;
+  margin: auto;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  cursor: pointer;
+  opacity: 0.4;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+  height: 100%;
+`;
+
+const Image = styled.img`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <FaAngleLeft />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide key={item.id}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <FaAngleRight />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "src/img/addera.webp",
+    },
+  
+    {
+      id: 2,
+      img: "src/img/eno_30.webp",
+    },
+
+    {
+    id: 3,
+    img: "src/img/ibuflex.webp",
+    },
+  ];
+
+  //IMPORTANTE: imagens tiradas do site da Panvel, usadas apenas para fins de estudo e sem fins lucrativos.
+  
