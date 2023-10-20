c8a3a1bc8d74e457a7f0ad457e92303bc89d7a0e
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2023-10-20 03:16:35 -0300

    feat: feito cards extras

--- src/pages/MarketplacePage.jsx
+++ src/pages/MarketplacePage.jsx
@@ -3,16 +3,18 @@ import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
 import Title from "../components/Title";
 import Products from "../components/Products";
+import ExtraCardsInfo from "../components/ExtraCardsInfo";
 
 const MarketplacePage = () => {
   return (
     <div>
-    <Navbar/>
-    <Slider/>
-    <Title/>
-    <Products/>
+      <Navbar />
+      <Slider />
+      <Title />
+      <Products />
+      <ExtraCardsInfo />
     </div>
-  )
-}
+  );
+};
 
-export default MarketplacePage
+export default MarketplacePage;
