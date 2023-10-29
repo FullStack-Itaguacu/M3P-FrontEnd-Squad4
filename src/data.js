e240aae973f0b3a30e883aa96ff7abde4975f2dd
Leonardo Arena <leonardoarena7@gmail.com>
2023-10-29 19:35:54 -0300

    fix: tentando corrigir imagens do banner no deploy

--- src/data.js
+++ src/data.js
@@ -1,27 +1,27 @@
 export const sliderItems = [
     {
       id: 1,
-      img: "src/img/addera.webp",
+      img: "/src/img/addera.webp",
     },
   
     {
       id: 2,
-      img: "src/img/eno_30.webp",
+      img: "/src/img/eno_30.webp",
     },
 
     {
     id: 3,
-    img: "src/img/ibuflex.webp",
+    img: "/src/img/ibuflex.webp",
     },
 
     {
     id: 4,
-    img: "src/img/image00001.png",
+    img: "/src/img/image00001.png",
     },
 
     {
     id: 5,
-    img: "src/img/image00002.png",
+    img: "/src/img/image00002.png",
     }          
   ];
 
