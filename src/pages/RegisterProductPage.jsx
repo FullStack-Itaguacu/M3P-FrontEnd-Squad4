02484b2c75a707b14359dfbd0869008257bd1285
huitson-pereira <124617896+huitson-pereira@users.noreply.github.com>
2023-10-29 01:39:35 -0300

    feat: Adicionado o componente Sidebar na Página RegisterProductPage.jsx

--- src/pages/RegisterProductPage.jsx
+++ src/pages/RegisterProductPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import RegisterProduct from "../components/RegisterProduct";
 import ProductsRegistered from "../components/ProductsRegistered";
+import Sidebar from "../components/Sidebar";
 
 const Container =styled.div`
     border: none;
@@ -12,6 +13,7 @@ function RegisterProductPage(){
     
     return(
         <Container>
+            <Sidebar/>
             <RegisterProduct/>
             <ProductsRegistered/>
         </Container>
