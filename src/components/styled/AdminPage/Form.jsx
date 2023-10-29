ae1a892138dfca9705cff13f78784a7252369fc2
Leonardo Arena <leonardoarena7@gmail.com>
2023-10-29 14:07:17 -0300

    feat: styled component Form

---
+++ src/components/styled/AdminPage/Form.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Form = styled.form`
+  background-color: gray;
+  padding: 20px;
+  max-width: 70vw;
+  margin: 6vh auto 0 19vw;
+  border-radius: 25px;
+`;
+
+export default Form;
